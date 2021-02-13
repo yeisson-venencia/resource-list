265cab49557f821eee692144683cc6bfb598eef9
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 15:44:27 -0400

    p01/init: defining structure for resource panel

---
+++ client/src/components/resource-panel/ResourcePanel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <h1>List of Resources</h1>
+    </header>
+    <ul>
+      <resource-panel-item
+        v-for="res in storedResources"
+        :key="res.id"
+        :id="res.id"
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"
+      ></resource-panel-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    storedResources: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
