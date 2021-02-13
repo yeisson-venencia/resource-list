f99b9f56abd36f5e6c8299a04369eb5db7a92a9c
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 14:53:19 -0400

    p01/init: adding initial dummy data

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  storedResources: [
+    {
+      id: 'official-guide',
+      title: 'Official Guide',
+      description: 'the oficial VueJS documentation',
+      link: 'htt://vuejs.org'
+    },
+    {
+      id: 'google',
+      title: 'Google',
+      description: 'Learn to google..',
+      link: 'htt://google.com'
+    }
+  ]
+};
+</script>
