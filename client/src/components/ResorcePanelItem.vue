7b965b14a6efa3ecf2890b22a0074d29fa6da3da
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 15:10:32 -0400

    p01/init: creating resource item component structure

---
+++ client/src/components/ResorcePanelItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p></p>
+    <nav>
+      <a href="">Go to resource...</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    id: { id: Object, required: true },
+    description: { type: String, required: true },
+    link: { type: String, required: true }
+  }
+};
+</script>
