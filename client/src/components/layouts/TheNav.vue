b22786e5106c48445f64df4c1cdb6ef2a48e0bdf
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 18:47:44 -0400

    p03/form: changing the structure of nav component

--- client/src/components/layouts/TheNav.vue
+++ client/src/components/layouts/TheNav.vue
@@ -1,6 +1,26 @@
 <template>
-  <base-card>
-    <base-button>Resource List</base-button>
-    <base-button>Add Resources</base-button>
-  </base-card>
+  <div>
+    <base-card v-for="tab in tabs" :key="tab.component">
+      <base-button @click="changeTab(tab.component)">
+        {{ tab.label }}
+      </base-button>
+    </base-card>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    tabs: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ['change-content'],
+  methods: {
+    changeTab(tab) {
+      this.$emit('change-content', tab);
+    }
+  }
+};
+</script>
