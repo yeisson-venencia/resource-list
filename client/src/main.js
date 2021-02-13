c873a52819bd538e891ba1472f6db25bfae4b7ef
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 17:57:33 -0400

    p02/header: Adding the BaseButton component to the app globally

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import BaseCard from './components/base-component/BaseCard.vue';
+import BaseButton from './components/base-component/BaseButton.vue';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
