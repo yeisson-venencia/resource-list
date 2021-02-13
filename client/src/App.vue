c42187d1f2482f77037098de57fe3299baf76a52
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 15:23:40 -0400

    p01/init: adding resource items to the app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,38 @@
 <template>
-  <ul></ul>
+  <ul>
+    <resource-panel-item
+      v-for="res in storedResources"
+      :key="res.id"
+      :id="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></resource-panel-item>
+  </ul>
 </template>
 
 <script>
+import ResourcePanelItem from './components/ResorcePanelItem';
+
 export default {
-  storedResources: [
-    {
-      id: 'official-guide',
-      title: 'Official Guide',
-      description: 'the oficial VueJS documentation',
-      link: 'htt://vuejs.org'
-    },
-    {
-      id: 'google',
-      title: 'Google',
-      description: 'Learn to google..',
-      link: 'htt://google.com'
-    }
-  ]
+  components: { ResourcePanelItem },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'the oficial VueJS documentation',
+          link: 'htt://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google..',
+          link: 'htt://google.com'
+        }
+      ]
+    };
+  }
 };
 </script>
