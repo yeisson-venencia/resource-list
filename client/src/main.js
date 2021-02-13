316ba73cc62dca1aa23343146422f56254180701
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 19:47:28 -0400

    p03/form: registered globally content title component

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,12 @@ import App from './App.vue';
 
 import BaseCard from './components/base-component/BaseCard.vue';
 import BaseButton from './components/base-component/BaseButton.vue';
+import ContentTitle from './components/base-component/ContentTitle.vue';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('content-title', ContentTitle);
 
 app.mount('#app');
