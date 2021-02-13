bb221ea160b40a532af3d562f781949da10c30a0
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 19:39:27 -0400

    p03/form: created form structure

--- client/src/components/resource-form/AddResource.vue
+++ client/src/components/resource-form/AddResource.vue
@@ -1,3 +1,24 @@
 <template>
-  <h2>Add resources</h2>
+  <div>
+    <header>
+      <h1>List of Resources</h1>
+    </header>
+    <form action="">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" id="link" name="link" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </div>
 </template>
