ff90039388b79b3f66b58e8de26d1c8afe7f7b02
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 20:43:25 -0400

    p03/form: created dialog component

---
+++ client/src/components/base-component/BaseDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div></div>
+    <dialog open>
+      <header>
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
+      <section>
+        <slot> </slot>
+      </section>
+      <menu>
+        <slot name="actions"></slot>
+      </menu>
+    </dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: false, default: 'Error' }
+  }
+};
+</script>
